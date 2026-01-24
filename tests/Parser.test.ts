2a6a54ef8d7f0c00066d50445452fd3b157c8660
bpetermann <benjamin.petermann@gmx.at>
2026-01-24 12:05:38 +0100

    test: change parser tests

--- tests/Parser.test.ts
+++ tests/Parser.test.ts
@@ -1,25 +1,34 @@
-import { writeFileSync } from 'fs';
-import { join } from 'path';
 import { describe, expect, it } from 'vitest';
 import Parser from '../lib/Parser.js';
-import { createTempFile } from './helper.js';
 
 describe('Parser', () => {
-  it('leaves non-import lines untouched', () => {
-    const { root } = createTempFile('console.log("hello");');
+  it('returns no dependencies for code without imports', () => {
     const data = `console.log("hello");`;
-    const parser = new Parser(root, data);
-    const result = parser.parse();
-    expect(result).toBe(data);
+    const parser = new Parser();
+
+    const dependencies = parser.parse(data);
+
+    expect(dependencies).toEqual([]);
   });
 
-  it('resolves a relative import', () => {
-    const { root } = createTempFile('import foo from "./foo.js";');
-    writeFileSync(join(root, 'foo.js'), 'export const foo = 42;');
-
+  it('extracts a single relative import', () => {
     const data = `import foo from "./foo.js";`;
-    const parser = new Parser(root, data);
-    const result = parser.parse();
-    expect(result).toBe('export const foo = 42;');
+    const parser = new Parser();
+
+    const dependencies = parser.parse(data);
+
+    expect(dependencies).toEqual(['./foo.js']);
+  });
+
+  it('extracts multiple imports', () => {
+    const data = `
+      import foo from "./foo.js";
+      import bar from "./bar.js";
+    `;
+    const parser = new Parser();
+
+    const dependencies = parser.parse(data);
+
+    expect(dependencies).toEqual(['./foo.js', './bar.js']);
   });
 });
