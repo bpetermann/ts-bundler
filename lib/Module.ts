f88f58d02841e36ad1a54b87344baf9c894092c1
bpetermann <benjamin.petermann@gmx.at>
2026-01-31 12:39:14 +0100

    feat: add mdoule class

---
+++ lib/Module.ts
@@ -0,0 +1,33 @@
+import { readFileSync } from 'fs';
+import Parser from './Parser.js';
+
+export default class Module {
+  filepath: string;
+  code?: string = undefined;
+  dependencies: string[] = [];
+  parser: Parser;
+
+  constructor(parser: Parser, file: string) {
+    this.parser = parser;
+    this.filepath = file;
+    this.build();
+  }
+
+  build() {
+    const data = this.readFile(this.filepath);
+    const dependencies = this.parser.parse(data);
+    this.dependencies = dependencies;
+    this.code = data;
+  }
+
+  readFile(filePath: string): string {
+    try {
+      return readFileSync(filePath, {
+        encoding: 'utf8',
+        flag: 'r',
+      });
+    } catch (err) {
+      throw Error(err.message);
+    }
+  }
+}
