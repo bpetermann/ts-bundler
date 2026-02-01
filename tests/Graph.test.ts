49836d9ff711794ad2457847387232a8bbd756b8
bpetermann <benjamin.petermann@gmx.at>
2026-02-01 20:34:36 +0100

    test: add graph dependencies test

--- tests/Graph.test.ts
+++ tests/Graph.test.ts
@@ -24,4 +24,44 @@ describe('Graph', () => {
     expect(files.some((f) => f.endsWith('index.js'))).toBe(true);
     expect(files.some((f) => f.endsWith('foo.js'))).toBe(true);
   });
+
+  it('deduplicates shared dependencies', () => {
+    const { root } = createTempFiles([
+      {
+        file: 'index.js',
+        text: `
+          import sum from './bar.js';
+          import add from './foo.js';
+        `,
+      },
+      {
+        file: 'foo.js',
+        text: `
+          import sum from './bar.js';
+
+          export default function add(a, b) {
+            return sum(a, b);
+          }
+        `,
+      },
+      {
+        file: 'bar.js',
+        text: `
+          export default function sum(a, b) {
+            return a + b;
+          }
+        `,
+      },
+    ]);
+
+    const graph = new Graph(root, 'index.js');
+
+    expect(graph.dependencyGraph).toHaveLength(3);
+
+    const files = graph.dependencyGraph.map((m) => m.filepath);
+
+    expect(files.filter((f) => f.endsWith('index.js'))).toHaveLength(1);
+    expect(files.filter((f) => f.endsWith('foo.js'))).toHaveLength(1);
+    expect(files.filter((f) => f.endsWith('bar.js'))).toHaveLength(1);
+  });
 });
