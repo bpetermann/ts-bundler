9c7a5d53f92055f5460124f4f50bb425c2a1213f
bpetermann <benjamin.petermann@gmx.at>
2026-02-04 22:20:58 +0100

    feat: add runtime class

---
+++ lib/Runtime.ts
@@ -0,0 +1,29 @@
+export const REQUIRE = '__require__' as const;
+
+export default class Runtime {
+  constructor(private readonly entryFile: string) {}
+
+  createRuntimeLoader(): string {
+    return `${this.createRequireFunction()}${this.emitEntryExecution()}`;
+  }
+
+  private createRequireFunction(): string {
+    return `
+  const cache = {};
+
+  function ${REQUIRE}(id) {
+    if (cache[id]) return cache[id].exports;
+
+    const module = { exports: {} };
+    cache[id] = module;
+
+    graph[id](${REQUIRE}, module, module.exports);
+    return module.exports;
+  }
+  `;
+  }
+
+  private emitEntryExecution(): string {
+    return `const entry = './' + ${JSON.stringify(this.entryFile)}; ${REQUIRE}(entry);`;
+  }
+}
